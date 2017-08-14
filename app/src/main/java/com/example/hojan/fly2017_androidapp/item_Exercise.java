38833033415fab6ca4bbdb38dd80858b2e0e37ce
NamJuPark <qkrska66@hanyang.ac.kr>
2017-08-15 00:29:33 +0900

    nam_Add category. 8.15

---
+++ app/src/main/java/com/example/hojan/fly2017_androidapp/item_Exercise.java
@@ -0,0 +1,17 @@
+package com.example.hojan.fly2017_androidapp;
+
+/**
+ * Created by 박남주 on 2017-08-14.
+ */
+
+public class item_Exercise {
+    int exerno;
+    String exerName;
+    String exerCaption;
+
+    public item_Exercise(int exerno, String exerName, String exerCaption){
+        this.exerno = exerno;
+        this.exerName = exerName;
+        this.exerCaption = exerCaption;
+    }
+}
---
+++ app/src/main/java/com/example/hojan/fly2017_androidapp/item_Exercise_Category.java
@@ -0,0 +1,13 @@
+package com.example.hojan.fly2017_androidapp;
+
+/**
+ * Created by 박남주 on 2017-08-12.
+ */
+
+public class item_Exercise_Category {
+    int imgno;
+
+    public item_Exercise_Category(int imgno){
+        this.imgno = imgno;
+    }
+}
